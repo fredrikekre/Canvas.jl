6c908e0f8afe7b7bd3539eb9b8619aeda8820dcb
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-12-03 09:56:12 +0000

    build based on 3816356

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#**1.**-Introduction-1","page":"1. Introduction","title":"1. Introduction","text":"","category":"section"},{"location":"#","page":"1. Introduction","title":"1. Introduction","text":"Welcome to the documentation for Canvas.jl – a Julia interface to the Canvas LMS","category":"page"}]
+}
